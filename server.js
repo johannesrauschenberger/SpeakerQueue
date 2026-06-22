5d4cfa6613ff2a308b1f4416f03ac13b2e4481bd
Johannes Rauschenberger <johannesrauschenberger@gmail.com>
2026-06-22 17:18:00 +0200

    Checkpoint 16 - Render deployment config

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const meetings = {};
 
 app.use(express.urlencoded({ extended: true }));
